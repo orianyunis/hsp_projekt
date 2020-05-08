4dc528e65b92db006c36ebaa33130497a0c41a63
Dieudonne-92 <64908700+Dieudonne-92@users.noreply.github.com>
2020-05-08 12:36:07 +0200

    Flächenschwerpunkt

--- HSP_Projekt/HSP_Projekt/Program.cs
+++ HSP_Projekt/HSP_Projekt/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            double hoehe, breit, flaeche, iY, iX;
-            Console.WriteLine("Bitte, Informieren Sie die Höhe und Breit Ihres Rechtecksprofil");
+            double hoehe, breit, flaeche, iY, iX, xS, yS;
+            Console.WriteLine("Bitte, Informieren Sie die Höhe und die Breite Ihres Rechtecksprofils");
             Console.WriteLine("Bitte Höhe eingeben:");
             hoehe = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Bitte Breit eingeben:");
@@ -22,25 +22,23 @@ namespace ConsoleApp3
                 Console.Clear();
                 ///Fläche////
                 flaeche = hoehe * breit;
-                Console.WriteLine("Die Fläche ihres Rechtecksprofil ist " + flaeche + " .");
+                Console.WriteLine("Die Fläche ihres Rechtecksprofils ist " + flaeche + " .");
                 ///Flächenträgheitsmoment
                 iY = (breit * (hoehe * hoehe * hoehe)) / 12;
                 iX = (hoehe * (breit * breit * breit)) / 12;
 
-                Console.WriteLine("Die Flächenträgheitsmoment ihres Rechtecksprofil sind für Iy = " + iY + ". Für Ix = " + iX + " .");
-
-
+                Console.WriteLine("Die Flächenträgheitsmoment ihres Rechtecksprofils sind für Iy = " + iY + ". Für Ix = " + iX + " .");
+                ///Flächenschwerpunkt
+                xS = breit / 2;
+                yS = hoehe / 2;
+                Console.WriteLine("Die Flächenschwerpunkt ihres Rechtecksprofils sind für Xs = " + xS + ". Für Ys = " + yS + " .");
             }
-
+            else
+            {
+                Console.WriteLine("Bitte das Programm neue Starten und eine gültige Nummer eingeben.");
+            }
 
             Console.ReadKey();
         }
     }
-}
-
-
-
-
-
-
-
+    }
